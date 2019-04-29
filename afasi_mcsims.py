4d678cbc50037724e018c77a68631ca7e6719c66
Phatak, Charudatta M <c.phatak@gmail.com>
2019-04-29 16:35:31 -0500

    Updated init_lattice to use cKDTree for NN search for faster speed. Started MC_move function for MC sims.

--- afasi_mcsims.py
+++ afasi_mcsims.py
@@ -13,6 +13,18 @@ import os as os
 import sys as sys
 import datetime
 import time as time
+from scipy.spatial import cKDTree as sp_cKDTree
+from matplotlib import pyplot as plt
+
+#------------------------------------------------------------------
+#
+# Function using the kdtree algorithm to find the nearest neighbors.
+# USing the method described here - https://stackoverflow.com/questions/10818546/finding-index-of-nearest-point-in-numpy-arrays-of-x-and-y-coordinates
+#
+def do_kdtree(combined_x_y_arrays,points,max_nn,max_dd):
+    mytree = sp_cKDTree(combined_x_y_arrays)
+    dist, indexes = mytree.query(points, k=max_nn, distance_upper_bound=max_dd)
+    return indexes
 
 #------------------------------------------------------------------
 #
@@ -65,21 +77,10 @@ def init_afasi_latt(a = 350, # lattice parameter
             #increment count
             count += 6
 
-    # next we calculate the distance map for the lattice
-    distmap = np.zeros([n_isl,n_isl])
-    for i in range(n_isl):
-        for j in range(n_isl):
-            distmap[i,j] = np.sqrt((centers[0,i]-centers[0,j])**2 +
-                                   (centers[1,i]-centers[1,j])**2)
-
-    # next we go through each island and sort the indices of the nearest neighbors.
-    nn_inds = np.zeros([n_isl,max_nn_num])
-    for i in range(n_isl):
-        inds = np.argsort(distmap[i,:])
-        for cnt in range(1,max_nn_num+1):
-            j = inds[cnt]
-            if (distmap[i,j] <= max_nn_dist):
-                nn_inds[i,cnt-1] = j
+    #now to use the cKDTree method
+    comb_xy = centers.transpose()
+    p_list = list(comb_xy)
+    nn_inds = do_kdtree(comb_xy,p_list,max_nn_num+1,max_nn_dist)
 
     return [centers,angles,nn_inds]
 #------------------------------------------------------------------
@@ -99,9 +100,9 @@ def calc_energy(centers = 0, # array of centers of islands
 
     # loop over each island and compute the neighbors
     for i in range(n_isl):
-        for cnt in range(num_neighbors):
-            j = nn_inds[i,cnt].astype('int')
-            if (i != j):
+        for cnt in range(num_neighbors-1):
+            j = nn_inds[i,cnt+1].astype('int')
+            if ((i != j) and (j != n_isl)):
                 si_sj = mag[0,i]*mag[0,j] + mag[1,i]*mag[1,j]
                 r_ij = np.sqrt((centers[0,i]-centers[0,j])**2 +
                                (centers[1,i]-centers[1,j])**2)
@@ -116,13 +117,71 @@ def calc_energy(centers = 0, # array of centers of islands
 
 #------------------------------------------------------------------
 #
-# the main function for running the MC simulations.
+# MC_move function to actual run the MC simulation.
+#
+# This function will take the input parameters for the MC simulation
+# then for number of MC iters, randomly select a spin site and flip its
+# direction, calculate energy and check whether to accept it or not.
+# It will also calculate the thermodynamic parameters such as sp. heat,
+# and susceptibility during the MC iters for a given temperature.
+#
+def MC_move(centers = 0, #centers of islands
+            mag = 0, #magnetization of islands
+            nn_inds = 0, #array for holding nearest neighbors
+            mc_iters = 10000, #number of MC iters per temp. step
+            eq_iters = 0, #number of equilibriation iterations.
+            temp = 700 #temperature variable for MC run
+            ):
+
+    #initialize arrays for holding various quantities
+    avg_en = 0.0
+    avg_en2 = 0.0
+    avg_mag = 0.0
+    avg_mag2 = 0.0
+    n_lowaccept = 0
+    n_highaccept = 0
+    n_noaccept = 0
+    
+    #get current energy
+    curr_energy = Calc_Energy(centers = centers, mag = mag, nn_inds = nn_inds)
+    
+    #beta_value
+    mu0 = 4.0*!pi*1e-7
+    mult_fac =
+
+    #num of islands
+    temp, n_isl = centers.shape
+
+    for nn in range(mc_iters):
+        for ii in range(n_isl):
+            # pick a random site in the lattice.
+            site = np.random.randint(0,n_isl)
+
+            #change the magnetization
+            mag[*,site] *= (-1)
+
+            #calculate the energy
+            new_energy = Calc_Energy(centers = centers, mag = mag, nn_inds = nn_inds)
+
+            #difference in energy
+            dE = new_energy - curr_energy
+
+            #check if we should accept this energy or not
+            if (dE lt 0):
+                n_lowaccept += 1
+                curr_energy = new_energy
+
+
+#
+#------------------------------------------------------------------
+#
+# the main function for defining parameters for MC simulations.
 
 #def afasi_mcrun(jobID = 'run1',
 a = 350.0 #lattice parameter
 s = 150.0 #island separation
-nx = 11 #repeat along x
-ny = 11 #repeat along y,
+nx = 5 #repeat along x
+ny = 5 #repeat along y,
 mc_iters = 1000 #number of MC iterations
 eq_iters = 0 #number of equilibriation iterations
 start_temp = 1000 #Start temperature
@@ -140,6 +199,7 @@ max_nn_dist = 1.5 * a
 #Compute number of islands.
 #6 islands per motif.
 n_isl = nx * ny * 6
+print('Number of islands:',n_isl)
 
 centers = np.zeros([2,n_isl])
 angles = np.zeros([n_isl])
@@ -149,13 +209,13 @@ nn_inds = np.zeros([n_isl,max_nn_num])
                           max_nn_num = max_nn_num)#, centers = centers, angles = angles, nn_inds = nn_inds)
 
 
-#next we compute the distance matrix.
-distmap = np.zeros([n_isl,n_isl])
-for i in range(n_isl):
-    for j in range(n_isl):
-        distmap[i,j] = np.sqrt((centers[0,i]-centers[0,j])**2 +
-                               (centers[1,i]-centers[1,j])**2)
-
+##next we compute the distance matrix.
+#distmap = np.zeros([n_isl,n_isl])
+#for i in range(n_isl):
+#    for j in range(n_isl):
+#        distmap[i,j] = np.sqrt((centers[0,i]-centers[0,j])**2 +
+#                               (centers[1,i]-centers[1,j])**2)
+#
 #next we define the magnetization array
 mag = np.zeros([2,n_isl])
 mag[0,:] = np.cos(np.deg2rad(angles))
