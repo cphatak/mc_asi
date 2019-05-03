a3bb30de4c13aec3122a71928a41022357482b90
Charudatta Phatak <c.phatak@gmail.com>
2019-05-02 23:29:54 -0500

    Changed the core MC simulation to a class file to define a dipolar MC object. Properties include all parameters and methods include computation of energy and MC_Move for a given temperature. Modified the afasi_mcsims to use the object file.

--- afasi_mcsims.py
+++ afasi_mcsims.py
@@ -6,6 +6,7 @@
 #
 # Written, CD Phatak, ANL, 21.Feb.2019.
 #
+# Modified to use the Dipolar_MC Class file.
 
 #import necessary modules
 import numpy as np
@@ -15,6 +16,7 @@ import datetime
 import time as time
 from scipy.spatial import cKDTree as sp_cKDTree
 from matplotlib import pyplot as plt
+from dipolar_MC import Dipolar_MC
 
 #------------------------------------------------------------------
 #
@@ -83,114 +85,25 @@ def init_afasi_latt(a = 350, # lattice parameter
     nn_inds = do_kdtree(comb_xy,p_list,max_nn_num+1,max_nn_dist)
 
     return [centers,angles,nn_inds]
-#------------------------------------------------------------------
-#
-# Calc_Energy function for AFASI
-#
-def calc_energy(centers = 0, # array of centers of islands
-                mag = 0, # array of magnetization of islands
-                nn_inds = 0 # array for nearest neighbors for dipolar interaction.
-                ):
-
-    # number of islands
-    n_isl,num_neighbors = nn_inds.shape
-
-    # Energy variable
-    tot_energy = 0
-
-    # loop over each island and compute the neighbors
-    for i in range(n_isl):
-        for cnt in range(num_neighbors-1):
-            j = nn_inds[i,cnt+1].astype('int')
-            if ((i != j) and (j != n_isl)):
-                si_sj = mag[0,i]*mag[0,j] + mag[1,i]*mag[1,j]
-                r_ij = np.sqrt((centers[0,i]-centers[0,j])**2 +
-                               (centers[1,i]-centers[1,j])**2)
-                if (r_ij == 0):
-                        print(i,j)
-                si_rij = (centers[0,i]-centers[0,j])*mag[0,i] + (centers[1,i]-centers[1,j])*mag[1,i]
-                sj_rji = (centers[0,j]-centers[0,i])*mag[0,j] + (centers[1,j]-centers[1,i])*mag[1,j]
-                tot_energy += (si_sj/r_ij**3 - (3.0*si_rij*sj_rji)/r_ij**5)
-
-    #return total energy
-    return tot_energy/2.0
-
-#------------------------------------------------------------------
-#
-# MC_move function to actual run the MC simulation.
-#
-# This function will take the input parameters for the MC simulation
-# then for number of MC iters, randomly select a spin site and flip its
-# direction, calculate energy and check whether to accept it or not.
-# It will also calculate the thermodynamic parameters such as sp. heat,
-# and susceptibility during the MC iters for a given temperature.
-#
-def MC_move(centers = 0, #centers of islands
-            mag = 0, #magnetization of islands
-            nn_inds = 0, #array for holding nearest neighbors
-            mc_iters = 10000, #number of MC iters per temp. step
-            eq_iters = 0, #number of equilibriation iterations.
-            temp = 700 #temperature variable for MC run
-            ):
-
-    #initialize arrays for holding various quantities
-    avg_en = 0.0
-    avg_en2 = 0.0
-    avg_mag = 0.0
-    avg_mag2 = 0.0
-    n_lowaccept = 0
-    n_highaccept = 0
-    n_noaccept = 0
-    
-    #get current energy
-    curr_energy = Calc_Energy(centers = centers, mag = mag, nn_inds = nn_inds)
-    
-    #beta_value
-    mu0 = 4.0*!pi*1e-7
-    mult_fac =
-
-    #num of islands
-    temp, n_isl = centers.shape
-
-    for nn in range(mc_iters):
-        for ii in range(n_isl):
-            # pick a random site in the lattice.
-            site = np.random.randint(0,n_isl)
-
-            #change the magnetization
-            mag[*,site] *= (-1)
-
-            #calculate the energy
-            new_energy = Calc_Energy(centers = centers, mag = mag, nn_inds = nn_inds)
-
-            #difference in energy
-            dE = new_energy - curr_energy
-
-            #check if we should accept this energy or not
-            if (dE lt 0):
-                n_lowaccept += 1
-                curr_energy = new_energy
-
 
 #
 #------------------------------------------------------------------
 #
 # the main function for defining parameters for MC simulations.
 
-#def afasi_mcrun(jobID = 'run1',
+jobID = 'run1',
 a = 350.0 #lattice parameter
 s = 150.0 #island separation
 nx = 5 #repeat along x
 ny = 5 #repeat along y,
 mc_iters = 1000 #number of MC iterations
 eq_iters = 0 #number of equilibriation iterations
-start_temp = 1000 #Start temperature
+start_temp = 100 #Start temperature
 end_temp = 1 #end temperature
 red_fac = 0.95 #reduction factor
 save_file = 500 #save config data during MC runs
 verbose = True
 display = False
-    #                ):
 
 #Set the next nearest neghbors
 max_nn_num = 9
@@ -204,25 +117,26 @@ print('Number of islands:',n_isl)
 centers = np.zeros([2,n_isl])
 angles = np.zeros([n_isl])
 nn_inds = np.zeros([n_isl,max_nn_num])
+
 #next we initialize the lattice.
 [centers,angles,nn_inds] = init_afasi_latt(a = a, s = s, nx = nx, ny = ny, max_nn_dist = max_nn_dist,
                           max_nn_num = max_nn_num)#, centers = centers, angles = angles, nn_inds = nn_inds)
 
+#next we initialize the Dipolar_MC Class for MC sims
+dipolar_MC1 = Dipolar_MC(centers = centers, angles = angles, nn_inds = nn_inds,
+                         max_nn_num = max_nn_num, max_nn_dist = max_nn_dist)
 
-##next we compute the distance matrix.
-#distmap = np.zeros([n_isl,n_isl])
-#for i in range(n_isl):
-#    for j in range(n_isl):
-#        distmap[i,j] = np.sqrt((centers[0,i]-centers[0,j])**2 +
-#                               (centers[1,i]-centers[1,j])**2)
-#
-#next we define the magnetization array
-mag = np.zeros([2,n_isl])
-mag[0,:] = np.cos(np.deg2rad(angles))
-mag[1,:] = np.sin(np.deg2rad(angles))
-
-#next we calculate energy
-curr_energy = calc_energy(centers = centers, mag = mag, nn_inds = nn_inds)
-
+res = dipolar_MC1.Calc_Energy()
+print("Current Energy:",dipolar_MC1.energy)
 
 #compute total number of temperature steps
+#checking how long one MC run at high temperature takes.
+dipolar_MC1.mc_iters = 100
+dipolar_MC1.eq_iters = eq_iters
+dipolar_MC1.temp = 0.1
+
+start = time.time()
+res = dipolar_MC1.MC_move(verbose=True)
+end = time.time()
+print("Runtime :",end-start)
+print("End energy:",dipolar_MC1.energy)
---
+++ dipolar_MC.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python
+#
+# Python Class file for Dipolar MC
+#
+# The purpose of this class file is to create an object
+# for performing Monte Carlo Simulations that are based
+# on dipolar energy interactions as the main energy term
+#
+# Written by CD Phatak, ANL, 02.May.2019.
+
+#import necessary modules
+import numpy as np
+import os as os
+import scipy.constants as physcon
+import datetime
+import time as time
+
+class Dipolar_MC(object):
+
+    def __init__(self,
+                 centers = 0, #array of centers of each islands
+                 angles = 0, #array of angles for each island
+                 nn_inds = 0, #array of indices for nearest neighbors
+                 max_nn_num = 1, #max. number of neighbors to consider
+                 max_nn_dist = 1, #max. distance for nearest neighbor
+                 verbose=False):
+        #This function is for initializing the dipolar_MC object
+        #The above parameters can be set while initializing and others can
+        #be set below.
+        
+        #parameters for describing lattice
+        self.centers = centers
+        self.angles = angles
+        self.nn_inds = nn_inds
+        self.max_nn_num = max_nn_num
+        self.max_nn_dist = max_nn_dist
+        
+        #MC simulation parameters - default values
+        #they can be changed after definition.
+        self.mc_iters = 1000 #total MC iters
+        self.eq_iters = 0 #number of iters for equilbriation before computing
+        self.temp = 10 #dimensionless temperature parameter
+        
+        #other derived parameters
+        temp, n_isl = centers.shape
+        self.n_isl = n_isl #numner of islands
+        #magnetization parameter
+        self.magx = np.cos(np.deg2rad(angles))
+        self.magy = np.sin(np.deg2rad(angles))
+        
+        #parameters for storing various values
+        self.n_lowaccept = 0
+        self.n_highaccept = 0
+        self.n_noaccept = 0
+        self.energy = 0
+        self.avgenergy = 0
+        self.netmag = np.sqrt(np.sum(self.magx**2) + np.sum(self.magy**2))
+        self.sp_heat = 0
+        self.suscep = 0
+        
+        #print output message
+        print("Created the Dipolar_MC class.")
+        print("Please run self.Calc_Energy to update self energy")
+
+
+    #------------------------------------------------------------------
+    #
+    # Calc_Energy function for AFASI
+    #
+    def Calc_Energy(self):
+    
+        # Energy variable
+        tot_energy = 0
+    
+        # loop over each island and compute the neighbors
+        for i in range(self.n_isl):
+            for cnt in range(self.max_nn_num-1):
+                j = self.nn_inds[i,cnt+1].astype('int')
+                if ((i != j) and (j != self.n_isl)):
+                    
+                    si_sj = self.magx[i]*self.magy[j] + self.magx[i]*self.magy[j]
+                    
+                    r_ij = np.sqrt((self.centers[0,i]-self.centers[0,j])**2 +
+                                   (self.centers[1,i]-self.centers[1,j])**2)
+                    
+                    si_rij = (self.centers[0,i]-self.centers[0,j])*self.magx[i] 
+                    + (self.centers[1,i]-self.centers[1,j])*self.magy[i]
+                    
+                    sj_rji = (self.centers[0,j]-self.centers[0,i])*self.magx[j] 
+                    + (self.centers[1,j]-self.centers[1,i])*self.magy[j]
+                    
+                    tot_energy += (si_sj/r_ij**3 - (3.0*si_rij*sj_rji)/r_ij**5)
+    
+        #return total energy
+        self.energy = tot_energy/2.0
+        return 1
+
+    #------------------------------------------------------------------
+    #
+    # MC_move function to actual run the MC simulation.
+    #
+    # This function will take the input parameters for the MC simulation
+    # then for number of MC iters, randomly select a spin site and flip its
+    # direction, calculate energy and check whether to accept it or not.
+    # It will also calculate the thermodynamic parameters such as sp. heat,
+    # and susceptibility during the MC iters for a given temperature.
+    #
+    def MC_move(self,verbose=False):
+    
+        #initialize arrays for holding various quantities
+        avg_en = 0.0
+        avg_en2 = 0.0
+        avg_mag = 0.0
+        avg_mag2 = 0.0
+        
+        #get current energy
+        self.energy = self.Calc_Energy()
+    
+        for nn in range(self.mc_iters):
+            if (verbose):
+                if (np.mod(nn,10) == 0):
+                    print(nn)
+            for ii in range(self.n_isl):
+                # pick a random site in the lattice.
+                site = np.random.randint(0,self.n_isl)
+    
+                #change the magnetization
+                self.magx[site] *= (-1)
+                self.magy[site] *= (-1)
+    
+                #calculate the energy
+                new_energy = self.Calc_Energy()
+    
+                #difference in energy
+                dE = new_energy - self.energy
+    
+                #check if we should accept this energy or not
+                if (dE < 0):
+                    self.n_lowaccept += 1
+                    self.energy = new_energy
+                
+                if (dE > 0):
+                    #we check if we should accept the high energy change
+                    if (np.random.random_sample() < np.exp(-dE/self.temp)):
+                        self.n_highaccept += 1
+                        self.energy = new_energy
+                    else:
+                        #we do not accept the change
+                        self.magx[site] *= (-1)
+                        self.magy[site] *= (-1)
+                        self.n_noaccept += 1
+                    
+                #Next we start computing various thermo. terms
+                self.netmag = np.sqrt(np.sum(self.magx**2) + np.sum(self.magy**2))
+                
+                if (nn >= self.eq_iters):
+                    avg_en += self.energy
+                    avg_en2 += self.energy**2
+                    avg_mag += self.netmag
+                    avg_mag2 += self.netmag**2
+                
+        #we are out of the MC loop.
+        #collect all the data.
+        cn = 1.0/((self.mc_iters-self.eq_iters)*self.n_isl)
+        self.avgenergy = avg_en*cn
+        self.netmag = avg_mag*cn
+        self.sp_heat = (avg_en2*cn - avg_en*avg_en*cn**2)/self.temp**2
+        self.suscep = (avg_mag2*cn - avg_mag*avg_mag*cn**2)/self.temp
+        
+        #print some output
+        print("MC iter complete at temp:",self.temp)
+        return 1
+
+
+## MAIN ##
+if __name__ == '__main__':
+    print(" Class definition for Dipolar_MC Class.")
