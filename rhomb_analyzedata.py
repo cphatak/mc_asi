313f3ba6b21a03cff68eb3e741413e0b5ed67847
Charudatta Phatak <c.phatak@gmail.com>
2020-10-26 22:36:20 -0500

    updating codes for rhombille simulations.

---
+++ rhomb_analyzedata.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 12:45:23 2019
+
+Script for aggregating the data resulting from the MC sims run in parallel
+We will average the data from all runs and save average, and std.dev values
+per temperature step.
+
+@author: cphatak
+"""
+import numpy as np
+from matplotlib import pyplot as plt
+import datetime
+import os
+from draw_lattice import draw_lattice
+
+def rhomb_analyzedata(a = 350, #lattice parameter
+                      tot_runs = 10, # total number of runs
+                      run_num = 1, #Run number to plot magnetization
+                      lattice_draw_step = 10, #draw lattice vector
+                      fldr = '12x12_set1/results_runs_data/',
+                      draw_col_lattice = False #drawing the islands with color map.
+                      ):
+
+    #---------------------------------------------------------------------
+    #directory base
+    subdir_base = 'run'
+    fname_base = 'Dipolar_MC1_'
+    magname_base = 'MCrun_mag_'
+    cenname_base = 'MCrun_lattice_coords_'
+    num_runs = tot_runs
+    
+    #results folder
+    #cwd = os.getcwd()
+    opdir = fldr + 'avg_results/'
+    
+    #create directory if it does not exist.
+    if not os.path.exists(opdir):
+        os.makedirs(opdir)
+    
+    
+    #number of header lines to skip
+    header_skip = 16
+    
+    #process all the data
+    fnames = []
+    for num_run in range(num_runs):
+        fnames.append(fldr + subdir_base + str(num_run) + '/' + 
+                      fname_base + subdir_base + str(num_run) + '.txt')
+        
+    
+    data = np.asarray([np.genfromtxt(filename,delimiter=',',skip_header=header_skip) for filename in fnames])
+    
+    
+    #compute the averages
+    avg_data = np.mean(data,axis=0)
+    stddev_data = np.std(data,axis=0)
+    
+    nvals, nvars = avg_data.shape
+    
+    #get the header from one of the files.
+    fname = fldr + subdir_base + str(run_num) + '/' + fname_base + subdir_base + str(run_num) + '.txt'
+    hdr=[]
+    with open(fname,'r') as reader:
+        for i in range(header_skip):
+            hdr.append(reader.readline())
+        
+    
+    
+    #save the averaged and stddev data
+    data_file = opdir + 'averaged_MC_data.txt'
+    f = open(data_file,"w+")
+    d = datetime.datetime.now()
+    f.write('# Averaged Data from MC runs.\n')
+    f.write('# Created: C. Phatak, ANL \n')
+    f.write('#{:%Y-%m-%d %H:%M:%S} \n'.format(d))
+    f.write('# Header from MC runs data follows: \n')
+    f.write(''.join(hdr))
+    for i in range(nvals):
+        f.write('{0:.4e}, {1:.4e}, {2:.3f}, {3:.4e}, {4:.4e}, {5:5f}, {6:5f}, {7:5f}\n'.format(avg_data[i,0],avg_data[i,1],avg_data[i,2],avg_data[i,3],avg_data[i,4],avg_data[i,5],avg_data[i,6],avg_data[i,7]))
+    
+    f.close()
+    
+    data_file = opdir + 'stddev_MC_data.txt'
+    f = open(data_file,"w+")
+    d = datetime.datetime.now()
+    f.write('# Std Dev Data from MC runs.\n')
+    f.write('# Created: C. Phatak, ANL \n')
+    f.write('#{:%Y-%m-%d %H:%M:%S} \n'.format(d))
+    f.write('# Header from MC runs data follows: \n')
+    f.write(''.join(hdr))
+    for i in range(nvals):
+        f.write('{0:.4e}, {1:.4e}, {2:.3f}, {3:.4e}, {4:.4e}, {5:5f}, {6:5f}, {7:5f}\n'.format(stddev_data[i,0],stddev_data[i,1],stddev_data[i,2],stddev_data[i,3],stddev_data[i,4],stddev_data[i,5],stddev_data[i,6],stddev_data[i,7]))
+    
+    f.close()
+    
+    #saving the magnetization vector maps.
+    #load the centers data
+    cen_file = fldr + subdir_base + str(run_num) + '/' + cenname_base + subdir_base + str(run_num)
+    centers = np.genfromtxt(cen_file + '.txt',delimiter=',',skip_header=2)
+    
+    for i in range(0,nvals,lattice_draw_step):
+        mag_file = fldr + subdir_base + str(run_num) + '/' + magname_base + subdir_base + str(run_num) + '_' + str(i)
+        mag_data = np.genfromtxt(mag_file + '.txt', delimiter=',', skip_header=1)
+        
+        #save the plot
+        fig, ax1 = plt.subplots(figsize=(8,8))
+        plt.rcParams['image.cmap'] = 'Paired'
+        ax1.set_title('a = {0:3d}, s = {1:3d}, T = {2:.4e}'.format(a,s,avg_data[i,0]))
+        col_arr = np.arctan2(mag_data[:,1],mag_data[:,0])
+        q1 = ax1.quiver(centers[:,0],centers[:,1],mag_data[:,0],mag_data[:,1],col_arr,pivot='mid',scale=25,headwidth=5)
+        #plt.draw()
+        plt.savefig(opdir+'Lattice_state_'+str(i)+'.jpg',bbox_inches='tight', dpi=150, quality=95)
+        plt.close()
+
+        #calling draw lattice.
+        blank = 0
+        if (draw_col_lattice):
+            res = draw_lattice(blank,cen_fname = cen_file, mag_fname = mag_file, dim = 1000, del_px = 15, Lx = 300, Ly = 100, thk = 10, save_tfs = False, save_lattice = True,
+                               buff_offset = 200)
+    
+    print('Averaging and Plotting complete.\n')
+    
+    #end
+    return 0
+
---
+++ rhomb_mcsims_par.py
@@ -0,0 +1,231 @@
+#
+#
+# This set of programs and routines are to perform Monte Carlo
+# simulations on the AF-ASI lattice using dipolar coupling as the
+# the dominant energy interactions.
+#
+# Written, CD Phatak, ANL, 21.Feb.2019.
+#
+# Modified to use the Dipolar_MC Class file.
+#
+# Modified, CD Phatak, ANL, 10.Jul.2019 - to use multiprocessing for parallel running.
+
+#import necessary modules
+import numpy as np
+import datetime
+import time as time
+import matplotlib as mpl
+#determine if linux
+import platform
+    #if (platform.system() == 'Linux'):
+mpl.use('Agg') # for linux operation.
+from matplotlib import pyplot as plt
+from dipolar_rhombille_MC import Dipolar_Rhomb_MC
+import os
+import multiprocessing as mp
+
+#------------------------------------------------------------------
+#
+# Printing out system information.
+#
+def print_sysinfo():
+    
+    print('\nPython version  :', platform.python_version())
+    print('compiler        :', platform.python_compiler())
+    
+    print('\nsystem     :', platform.system())
+    print('release    :', platform.release())
+    print('machine    :', platform.machine())
+    print('processor  :', platform.processor())
+    print('CPU count  :', mp.cpu_count())
+    print('interpreter:', platform.architecture()[0])
+    print('\n\n')
+
+
+#------------------------------------------------------------------
+#
+# the core function for running an individual MC simulation.
+def run_MC(n_run, #job ID number
+           a, #Lattice parameter
+           nx, #num of islands along x
+           ny, #num of islands along y
+           latt_type, #Type of Rhombille lattice.
+           mc_iters, #number of MC iterations
+           eq_iters, #number of equilibriation iterations
+           start_temp, #Start temperature
+           end_temp, #end temperature
+           n_temp, #number of temperature steps
+           red_fac, #reduction factor - 'Linear' or 'Geometric'
+           load_file, #load magnetization config data for MC runs
+           save_file, #to save files in intermediate MC steps. every N steps.
+           file_name, #filename for loading the data.
+           verbose,
+           work_dir, #working directory
+           display):
+    
+    #create the job ID
+    jobID = 'run'+str(n_run)
+    plt.ioff()
+    
+    #get current working directory
+    cwd = os.getcwd()
+    if not work_dir:
+        dir = cwd+'/'+jobID+'/'
+    else:
+        dir = work_dir + '/'+jobID+'/'
+
+    print('Saving to directory - '+dir)
+    
+    #create directory if it does not exist.
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    #Set the next nearest neghbors
+    max_nn_num = 9
+    max_nn_dist = 4 * a
+    
+    #variable for pair flip
+    pairflip = False
+
+    #Compute number of islands.
+    #6 islands per motif.
+    n_isl = nx * ny * 6
+    print('Number of islands:',n_isl)
+
+    #next we initialize the lattice.
+    dipolar_MC1 = Dipolar_Rhomb_MC(a = a, nx = nx, ny = ny, max_nn_dist = max_nn_dist,
+                            max_nn_num = max_nn_num, dir = dir, jobID = jobID, 
+                            latt_orient='rectangle', latt_type = latt_type,
+                            init_random = True)#, centers = centers, angles = angles, nn_inds = nn_inds)
+    
+    #check if we need to load previous data
+    if load_file:
+        mag_arr = np.genfromtxt(file_name, delimiter=',', skip_header=1)
+        dipolar_MC1.magx = mag_arr[:,0]
+        dipolar_MC1.magy = mag_arr[:,1]
+      
+    dipolar_MC1.energy = dipolar_MC1.Latt_Energy(debug=False)
+    current_energy = dipolar_MC1.energy
+    print("Current Energy:",dipolar_MC1.energy)
+    
+    #save the images if display option is set
+    if (display):
+    
+        fig1, ax1 = plt.subplots(figsize=(8,8))
+        ax1.set_title('MC sims initial state')
+        qq = ax1.quiver(dipolar_MC1.centers[0,:],dipolar_MC1.centers[1,:],dipolar_MC1.magx,dipolar_MC1.magy,pivot='mid')
+        #plt.draw()
+        plt.savefig(dir+'initial_state.png',bbox_inches='tight')
+        plt.close()
+    
+    #saving the centers and initial mag data
+    f1 = open(dir+'MCrun_lattice_coords_'+jobID+'.txt','w+')
+    f1.write('# Centers of the islands for lattice run \n')
+    f1.write('# Num islands {0:3d} \n'.format(dipolar_MC1.n_isl))
+    for i in range(dipolar_MC1.n_isl):
+        f1.write('{0:.3f}, {1:.3f} \n'.format(dipolar_MC1.centers[0,i],dipolar_MC1.centers[1,i]))
+
+    f1.close()
+
+    #save initial magnetization data
+    f2 = open(dir+'MCrun_mag_initial_'+jobID+'.txt', 'w+')
+    f2.write('# Num islands {0:3d} \n'.format(dipolar_MC1.n_isl))
+    for i in range(dipolar_MC1.n_isl):
+        f2.write('{0:.3f}, {1:.3f} \n'.format(dipolar_MC1.magx[i],dipolar_MC1.magy[i]))
+
+    f2.close()
+
+
+    #MC iterations
+    dipolar_MC1.mc_iters = mc_iters
+    dipolar_MC1.eq_iters = eq_iters
+
+    #compute total number of temperature steps
+    if (red_fac == 'Linear'):
+        temp_var = np.linspace(start_temp,end_temp,n_temp)
+
+    if (red_fac == 'Geometric'):
+        temp_var = np.geomspace(start_temp,end_temp,n_temp,endpoint=True)
+
+    ##open data file to save the runtime data
+    data_file = "Dipolar_MC1_"+jobID+".txt"
+    f = open(dir+data_file,"w+")
+    d = datetime.datetime.now()
+    f.write('# Runtime data file for variables.\n')
+    f.write('# Created: C. Phatak, ANL \n')
+    f.write('#{:%Y-%m-%d %H:%M:%S} \n'.format(d))
+    f.write('#\n')
+    #data dump of MC params
+    f.write('# a = {0:.1f}\n'.format(a))
+    f.write('# nx = {0:2d}\n'.format(nx))
+    f.write('# ny = {0:2d}\n'.format(ny))
+    f.write('# Latt Type = {0}\n'.format(latt_type))
+    f.write('# Num isl = {0:4d}\n'.format(n_isl))
+    f.write('# MC iters = {0:5d}\n'.format(mc_iters))
+    f.write('# EQ_iters = {0:5d}\n'.format(eq_iters))
+    f.write('# St. temp = {0:.4e}\n'.format(start_temp))
+    f.write('# End temp = {0:.4e}\n'.format(end_temp))
+    f.write('# Num. temp = {0:3d}\n'.format(n_temp))
+    f.write('#\n')
+    f.write('Temp,     Energy,     Mag,     Sp.Heat,    Susc.,  LowAccept,  HighAccept,   NoAccept\n')
+    f.close()
+
+    #compute runtime
+    start = time.time()
+    
+    #break counter
+    break_count = 0
+
+    #Start the sims
+    for i in range(n_temp):
+        dipolar_MC1.temp = temp_var[i]
+        dipolar_MC1.MC_move(verbose=verbose, pairflip=pairflip, save_file=save_file) #optional argument verbose.
+        f = open(dir+data_file,"a+")
+        f.write('{0:.4e}, {1:.4e}, {2:.3f}, {3:.4e}, {4:.4e}, {5:5d}, {6:5d}, {7:5d}\n'.format(dipolar_MC1.temp, dipolar_MC1.avgenergy, dipolar_MC1.netmag, dipolar_MC1.sp_heat, dipolar_MC1.suscep, dipolar_MC1.n_lowaccept, dipolar_MC1.n_highaccept, dipolar_MC1.n_noaccept))
+        f.close()
+        if verbose:
+            print(dipolar_MC1.temp, dipolar_MC1.avgenergy, dipolar_MC1.netmag, dipolar_MC1.sp_heat, dipolar_MC1.suscep, dipolar_MC1.n_highaccept, dipolar_MC1.n_lowaccept, dipolar_MC1.n_noaccept)
+        #save the draw lattice data
+        if (display):
+        
+            fig, ax1 = plt.subplots(figsize=(8,8))
+            ax1.set_title('Lattice State at {0:.3f}'.format(dipolar_MC1.temp))
+            q1 = ax1.quiver(dipolar_MC1.centers[0,:],dipolar_MC1.centers[1,:],dipolar_MC1.magx,dipolar_MC1.magy,pivot='mid')
+            #plt.draw()
+            plt.savefig(dir+'Lattice_state_'+str(i)+'.png',bbox_inches='tight')
+            plt.close()
+    
+        #save magnetization data
+        f2 = open(dir+'MCrun_mag_'+jobID+'_'+str(i)+'.txt','w+')
+        f2.write('# Num islands {0:3d} \n'.format(dipolar_MC1.n_isl))
+        for i in range(dipolar_MC1.n_isl):
+            f2.write('{0:.3f}, {1:.3f} \n'.format(dipolar_MC1.magx[i],dipolar_MC1.magy[i]))
+
+        f2.close()
+        
+        #Update current energy
+        current_energy = dipolar_MC1.avgenergy
+        
+        #Now we are checking for activating pair flip.
+        #If the total accepted changes are zero and net magnetization is zero, then we activate it.
+        #if (((dipolar_MC1.n_highaccept + dipolar_MC1.n_lowaccept) == 0) and (dipolar_MC1.netmag == 0)):
+        #    pairflip = True
+        #    print('Activated Pair Flip at temp {0:.4e}'.format(dipolar_MC1.temp))
+        
+        #check if the MC simulation has reached final state.
+        if (((dipolar_MC1.n_highaccept+dipolar_MC1.n_lowaccept) == 0) and ((dipolar_MC1.avgenergy - current_energy) == 0)):
+            break_count += 1
+            if (break_count > 5):
+                break
+        
+        
+    end = time.time()
+    print('Finished the run in time:',end-start)
+
+    return 1
+
+
+
+#------------------------------------------------------------------
+    
+    
