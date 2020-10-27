f10e2ccd033ac246e90d37984f74fdc0e157e61d
Phatak, Charudatta M <c.phatak@gmail.com>
2020-10-27 15:56:13 -0500

    Updated the runjob file for rhomb lattice.

--- rhomb_runjob.py
+++ rhomb_runjob.py
@@ -20,7 +20,7 @@ import os as os
 
 
 #Running for mltiple values of a and s
-a_vals = 350
+a_vals = np.asarray([350])
 latt_types = np.asarray(['dual_kagome'])
 n_a = a_vals.size
 n_l = latt_types.size
@@ -36,23 +36,23 @@ for ia in range(n_a):
         num_runs = 10 #Number of runs in parallel to perform.
         a = a_vals[ia]#350.0 #Lattice parameter
         latt_type = latt_types[jl] #type of lattice.
-        nx = 3 #num of islands along x
-        ny = 3 #num of islands along y
-        mc_iters = 50 #number of MC iterations
-        eq_iters = 0 #number of equilibriation iterations
-        start_temp = 10.0 #Start temperature
-        end_temp = 1.0 #end temperature
-        n_temp = 20 #number of temperature steps
-        red_fac = 'Linear' #reduction factor - 'Linear' or 'Geometric'
+        nx = 5 #num of islands along x
+        ny = 5 #num of islands along y
+        mc_iters = 1000 #number of MC iterations
+        eq_iters = 10 #number of equilibriation iterations
+        start_temp = 1.0e-5 #Start temperature
+        end_temp = 1.0e-11 #end temperature
+        n_temp = 300 #number of temperature steps
+        red_fac = 'Geometric' #reduction factor - 'Linear' or 'Geometric'
         save_file = 2000 #Save file every N steps in the MC iters.
         load_file = False #load magnetic config data during MC runs
         file_name = "MCrun_mag_run0_199.txt"
-        verbose = True
+        verbose = False
         display = True
         lattice_draw_step = 5
         
         #set working directory
-        work_dir = '/Users/cphatak/work/rhomb_sims/'+str(nx)+'x'+str(ny)+'_set'+str(set_num)
+        work_dir = '/Users/cphatak/ANL_work/spinice/rhomb_latt/dual_kag_MC/'+str(nx)+'x'+str(ny)+'_set'+str(set_num)
         
         #get system information
         print_sysinfo()
@@ -64,9 +64,9 @@ for ia in range(n_a):
         pool = mp.Pool(processes = nproc)
         results = [pool.apply_async(run_MC, args=(x, #job ID
                                                   a, #Lattice parameter
-                ****                                  s, #island separation
                                                   nx, #num of islands along x
                                                   ny, #num of islands along y
+                                                  latt_type, #Type of Rhombille lattice.
                                                   mc_iters, #number of MC iterations
                                                   eq_iters, #number of equilibriation iterations
                                                   start_temp, #Start temperature
@@ -85,11 +85,8 @@ for ia in range(n_a):
         print('Total runs completed:',nproc)
         
         #Once the run is complete, then we run analyze_data to average the results.
-        res = afasi_analyzedata(a = int(a), s = int(s), tot_runs = nproc, run_num = 0, lattice_draw_step = lattice_draw_step,
+        res = rhomb_analyzedata(a = int(a), tot_runs = nproc, run_num = 0, lattice_draw_step = lattice_draw_step,
                                 fldr = work_dir+'/')
-        
-        #Also run the Ising Pair analysis
-        res2 = afasi_isingpairs(a = int(a), s = int(s), run_num = 0, fldr = work_dir+'/', lattice_draw_step = lattice_draw_step)
         
         #then tar all the run output files.
         os.system("tar -zcf "+work_dir+"/results_runs_data.tar.gz "+work_dir+"/run*")
